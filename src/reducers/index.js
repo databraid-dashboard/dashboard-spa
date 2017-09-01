8173325d3e9e1debe96cf353e6a7c30f66d8be04
SSabu <sandeepsabu09@gmail.com>
2017-09-01 16:49:42 -0700

    Add dashboard with slack-widget

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import { rootReducer as transitReducer } from '@databraid/transit-widget/lib';
 import { rootReducer as githubReducer } from '@databraid/github-widget/lib';
+import { rootReducer as slackReducer } from '@databraid/slack-widget/lib';
 // remove eslint exception when slack widget is implemented
 /* eslint-disable no-unused-vars  */
 import { TRANSIT_WIDGET_ID, SLACK_WIDGET_ID, GITHUB_WIDGET_ID } from '../constants';
 
 const initialState = {
-  ids: [TRANSIT_WIDGET_ID, GITHUB_WIDGET_ID],
+  ids: [TRANSIT_WIDGET_ID, GITHUB_WIDGET_ID, SLACK_WIDGET_ID],
   byId: {},
 };
 
@@ -15,6 +16,7 @@ const widgets = (state = initialState, action) => ({
   byId: {
     [TRANSIT_WIDGET_ID]: transitReducer(state.byId[TRANSIT_WIDGET_ID], action),
     [GITHUB_WIDGET_ID]: githubReducer(state.byId[GITHUB_WIDGET_ID], action),
+    [SLACK_WIDGET_ID]: slackReducer(state.byId[SLACK_WIDGET_ID], action),
   },
 });
 
